0c2c2777508f8b39d8d57cb22b5a0d87fa9b2e1f
Nikita <professornikson@gmail.com>
2026-03-30 15:47:39 +0300

    refactor: routing return

--- src/graph/routing.py
+++ src/graph/routing.py
@@ -3,5 +3,6 @@ from src.domain.enums import ChartCaseType
 
 
 def route_case(state: PipelineState) -> str:
-    return "planning_non_canonical" if state[
-                                           "query_understanding"].case_type is ChartCaseType.NON_CANONICAL else "planning_canonical"
+    return "planning_non_canonical" \
+        if state["query_understanding"].case_type is ChartCaseType.NON_CANONICAL \
+        else "planning_canonical"
